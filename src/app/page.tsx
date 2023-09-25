de7afe0136c5f2b811200ba7bc69b8836c68f74d
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-25 12:00:08 -0700

    Converted TodoItem component to client component. Created update function to persist data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { TodoItem } from "@/components/TodoItem"
 import { prisma } from "@/db";
 import Link from "next/link";
 
 function getTodos() {
   return prisma.todo.findMany()
+}
+
+//Updates database of todos, server side
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+  await prisma.todo.update({ where: { id }, data: { complete }}) //data can persist
 }
 
 export default async function Home() {
@@ -18,7 +25,7 @@ export default async function Home() {
   </header>
   <ul className="pl-4">
     {todos.map(todo => (
-      <TodoItem key={todo.id} {...todo}/>
+      <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
     ))}
   </ul>
   </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,11 +1,29 @@
+//Default is server side, onChange implies interaction on client side, this converts to a client component
+"use client"
+
 type TodoItemProps = {
     id: string
     title: string
-    complete: Boolean
+    complete: boolean
+    toggleTodo: (id: string, complete: boolean) => void
 }
-export function TodoItem({ id, title, complete, }: TodoItemProps) {
-    return <li className="flex gap-1 items-center ">
-        <input id={id} type="checkbox" className="cursor-pointer peer"/>
-        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
-    </li>
-}
+export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
+    return (
+      <li className="flex gap-1 items-center">
+        <input
+          id={id}
+          type="checkbox"
+          className="cursor-pointer peer"
+          defaultChecked={complete}
+          onChange={e => toggleTodo(id, e.target.checked)}
+          //onChange implies interaction on client side, therefore this entire component must be converted to a client component
+        />
+        <label
+          htmlFor={id}
+          className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+        >
+          {title}
+        </label>
+      </li>
+    )
+  }
