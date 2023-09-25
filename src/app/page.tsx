e0ddc50f32708688770184a2a0f1115e1d78d323
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-24 19:22:42 -0700

    Styling todoItem component, completion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+import { prisma } from "@/db";
 import Link from "next/link";
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+export default async function Home() {
+  const todos  = await getTodos()
+
   return <>
   <header className="flex justify-between items-center mb-4">
     <h1 className="text-2xl">To-Dos</h1>
-    <Link href="/new">New</Link>
+    <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" 
+    href="/new">
+      New
+    </Link>
   </header>
-  <ul></ul>
+  <ul className="pl-4">
+    {todos.map(todo => (
+      <TodoItem key={todo.id} {...todo}/>
+    ))}
+  </ul>
   </>
 }
